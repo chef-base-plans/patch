def239dfedf82c5b7b7a69fb8626f709bdaa2ea3
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-09 12:04:36 +0100

    Add build status badge and update tests according to agreed pattern
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/patch_exists.rb
+++ controls/patch_exists.rb
@@ -1,17 +1,23 @@
 title 'Tests to confirm patch exists'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'patch')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-patch_relative_path = input('command_path', value: '/bin/patch')
-patch_installation_directory = command("hab pkg path #{plan_ident}")
-patch_full_path = patch_installation_directory.stdout.strip + "#{ patch_relative_path}"
- 
+
 control 'core-plans-patch-exists' do
   impact 1.0
   title 'Ensure patch exists'
   desc '
-  '
-   describe file(patch_full_path) do
+  Verify patch by ensuring /bin/patch exists'
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  command_relative_path = input('command_relative_path', value: '/bin/patch')
+  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  describe file(command_full_path) do
     it { should exist }
   end
 end
--- controls/patch_works.rb
+++ controls/patch_works.rb
@@ -1,24 +1,28 @@
 title 'Tests to confirm patch works as expected'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'patch')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
 
 control 'core-plans-patch-works' do
   impact 1.0
   title 'Ensure patch works as expected'
   desc '
+  Verify patch by ensuring (1) its installation directory exists and (2) that
+  it returns the expected version
   '
-  patch_path = command("hab pkg path #{plan_ident}")
-  describe patch_path do
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
   
-  patch_pkg_ident = ((patch_path.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  describe command("DEBUG=true; hab pkg exec #{ patch_pkg_ident} patch --version") do
+  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} patch --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /GNU patch 2.7.6/ }
+    its('stdout') { should match /GNU patch #{plan_pkg_version}/ }
     its('stderr') { should be_empty }
   end
 end
