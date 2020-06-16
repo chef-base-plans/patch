6f39bc0dc1455046610a032f51b72c60df601927
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-16 17:35:32 +0100

    Remove DEBUG and update according to pattern
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/patch_works.rb
+++ controls/patch_works.rb
@@ -7,19 +7,22 @@ control 'core-plans-patch-works' do
   impact 1.0
   title 'Ensure patch works as expected'
   desc '
-  Verify patch by ensuring (1) its installation directory exists and (2) that
-  it returns the expected version
+  Verify patch by ensuring that
+  (1) its installation directory exists 
+  (2) it returns the expected version
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
   end
   
-  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
-  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} patch --version") do
+  command_relative_path = input('command_relative_path', value: 'bin/patch')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  describe command("#{command_full_path} --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /GNU patch #{plan_pkg_version}/ }
