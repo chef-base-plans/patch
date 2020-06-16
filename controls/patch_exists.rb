193ca9c5ca08eb1c2b1b553f002635d97f6d0ebb
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-16 17:35:32 +0100

    Remove DEBUG and update according to pattern
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/patch_exists.rb
+++ controls/patch_exists.rb
@@ -7,16 +7,17 @@ control 'core-plans-patch-exists' do
   impact 1.0
   title 'Ensure patch exists'
   desc '
-  Verify patch by ensuring /bin/patch exists'
+  Verify patch by ensuring bin/patch exists'
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
   end
 
-  command_relative_path = input('command_relative_path', value: '/bin/patch')
-  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  command_relative_path = input('command_relative_path', value: 'bin/patch')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
   describe file(command_full_path) do
     it { should exist }
   end
